761d9e3fa4352918c372df937cbc386971fb7e79
Karim-Mansoor <126907306+karim-mansoor@users.noreply.github.com>
2026-04-10 23:10:43 +0100

    Reapply "installation [Test]"
    This reverts commit 2a8810578e32952ea22313823ebb83d8d1f8bab6.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { AboutUsComponent } from './Landing/about-us/about-us.component';
 import { ProductsComponent } from './Landing/products/products.component';
 import { ContactUsComponent } from './Landing/contact-us/contact-us.component';
 import { CartComponent } from './Landing/cart/cart.component';
+import { InstallationComponent } from './Landing/installation/installation.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'installation', component: InstallationComponent },
   { path: 'about', component: AboutUsComponent },
   { path: 'contact', component: ContactUsComponent },
   { path: 'cart', component: CartComponent }
-
 ];
